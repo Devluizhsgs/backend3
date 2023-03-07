547cdce2941665a016df332922f4fc1103a249a4
Luiz Humberto <111015369+Devluizhsgs@users.noreply.github.com>
2023-03-06 21:12:43 -0300

    Add files via upload

---
+++ backend3/src/test/java/br/iesp/edu/backend3/Backend3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.iesp.edu.backend3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
