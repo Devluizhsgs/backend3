71125f101c6de0e31de60c49071b43d8ec0fede2
Devluizhsgs <luizhsgs.dev@gmail.com>
2023-03-26 19:53:45 -0300

    Mapeamentos

--- vendas/src/main/java/br/iesp/edu/backend3/domain/entity/Pedido.java
+++ vendas/src/main/java/br/iesp/edu/backend3/domain/entity/Pedido.java
@@ -1,14 +1,37 @@
 package br.iesp.edu.backend3.domain.entity;
-
+import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity
+@Table(name = "pedido")
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+
+    @Column(name = "data_pedido")
     private LocalDate datapedido;
+
+    @Column(name = "total", length = 20, precision = 2)
     private BigDecimal total;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
 
     public Integer getId() {
         return id;
--- vendas/src/main/java/br/iesp/edu/backend3/domain/entity/Produto.java
+++ vendas/src/main/java/br/iesp/edu/backend3/domain/entity/Produto.java
@@ -1,11 +1,21 @@
 package br.iesp.edu.backend3.domain.entity;
 
+import jakarta.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "descricao")
     private String descicao;
+
+    @Column(name =  "preco_unitario")
     private BigDecimal preco;
 
     public Integer getId() {
