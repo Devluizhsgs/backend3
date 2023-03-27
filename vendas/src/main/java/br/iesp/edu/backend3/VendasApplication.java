e064e77486bfd15b303cd4e28702b7892ebcd539
Devluizhsgs <luizhsgs.dev@gmail.com>
2023-03-27 03:03:39 -0300

    Cadastro De Clientes

--- vendas/src/main/java/br/iesp/edu/backend3/VendasApplication.java
+++ vendas/src/main/java/br/iesp/edu/backend3/VendasApplication.java
@@ -1,47 +1,21 @@
 package br.iesp.edu.backend3;
 
 import br.iesp.edu.backend3.domain.entity.Cliente;
-import br.iesp.edu.backend3.domain.entity.Pedido;
 import br.iesp.edu.backend3.domain.repository.Clientes;
-import br.iesp.edu.backend3.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
 @SpringBootApplication
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos
-    ) {
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
         return args -> {
-            System.out.println("Salvando clientes");
-            Cliente fulano = new Cliente("Fulano");
-            clientes.save(fulano);
-
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDatapedido(LocalDate.now() );
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-            Cliente cliente = clientes.findClienteFetchPedido(fulano.getId());
-            System.out.println(cliente);
-            System.out.println(cliente.getPedidos());
-
-            pedidos.findbyCliente(fulano).forEach(System.out::println);
-
-
-
+            Cliente c = new Cliente(null, "Fulano");
+            clientes.save(c);
         };
     }
 
